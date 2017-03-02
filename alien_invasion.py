933fa8e61d0952c6339309da6e5c120766adf61b
Andrew Boychuk <kapman05@gmail.com>
2017-03-02 10:26:32 +0200

    alien_invasion refactored

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,28 +1,20 @@
-#Завершение игры по команде игрока
-import sys 
+import sys
 
-#Функциональность необходимая для созданиия игры
 import pygame
 
-from settings import Settings
-from ship import Ship
-import game_functions as gf
 
 def run_game():
-    #Инициализирует pygame, settings и объект экрана.
+    # Инициализирует игру и создает обьект экрана.
     pygame.init()
-    ai_settings = Settings()
-    screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_height))
+    screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Alien Invasion")
-    #Создание корабля
-    ship = Ship(screen)
 
-    #Назначение цвета фона.
-    bg_color = (230, 230, 230)
-
-    #Запуск основного цикла игры.
+    # Запуск основного цикла игры.
     while True:
-        gf.check_events()
-        gf.update_screen(ai_settings, screen, screen)
+        # Отслеживание событий клавиатуры и мыши.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        # Отображениу последнего прорисованого экрана.
+        pygame.display.flip()
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -2,16 +2,19 @@ import sys
 
 import pygame
 
+
 def check_events():
-#Отслеживание событий клавиатуры и мыши.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+    """Обрабатывает нажатия клавиш и события мыши."""
+for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        sys.exit()
+
 
 def update_screen(ai_settings, screen, ship):
     """Обновляет изображения на экране и отображает новый экран."""
-    #При каждом проходе цикла перерисовывается экран.
-    screen.fill(ai_settings.bg_color)
-    ship.blitme()
-    #Отображение последнего прорисованного экрана.
-    pygame.display.flip()
+# При каждом проходе цыкла перерисовывается экран.
+screen.fill(ai_settings.bg_color)
+ship.blitme()
+
+# Отображение последнего прорисованого экрана
+pygame.display.flip()
